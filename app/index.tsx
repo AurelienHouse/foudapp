1ac23da2117517f4c4b577503b6a74dfdd2a6aa4
AurelienHouse <aurelien.maison@gmail.com>
2024-04-21 03:11:02 +0200

    🎉 FoudApp: Create Project and copy restaurantsdata

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Redirect } from 'expo-router';
+import React from 'react';
+
+const Mainscreen = () => {
+  return <Redirect href="/home" />;
+};
+
+export default Mainscreen;
